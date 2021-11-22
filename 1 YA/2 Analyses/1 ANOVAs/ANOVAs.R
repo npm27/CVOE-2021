a1daa995735c2ecd79e9d2ef2ce00f28a2022fdc
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2021-11-22 15:45:27 -0600

    Run the ANOVAs

--- 1 YA/2 Analyses/1 ANOVAs/ANOVAs.R
+++ 1 YA/2 Analyses/1 ANOVAs/ANOVAs.R
@@ -1,139 +1,89 @@
 ##SET UP
-library(readxl)
 library(reshape)
 library(ez)
 
 options(scipen = 999)
 
 ####Start w/ younger####
-young_errors = read_xlsx("Data/YA Output 10_14.xlsx", sheet = "ERRORS")
-young_rts = read_xlsx("Data/YA Output 10_14.xlsx", sheet = "RTs")
-#young_Zrts = read_xlsx("Data/YA Output 10_14.xlsx", sheet = "zRTs")
-
-young_errors = young_errors[-c(78:nrow(young_errors)), ]
-young_rts = young_rts[-c(78:nrow(young_rts)), ]
-#young_Zrts = young_Zrts[-c(78:nrow(young_Zrts)), ]
-
-##add age group codings
-young_errors$age = rep("YA")
-young_rts$age = rep("YA")
-#young_Zrts$age = rep("YA")
-
-####Now do Healthies####
-healthy_errors = read_xlsx("Data/Older Healthy Output 10_14.xlsx", sheet = "Errors")
-healthy_rts = read_xlsx("Data/Older Healthy Output 10_14.xlsx", sheet = "RTs")
-#healthy_Zrts = read_xlsx("Data/Older Healthy Output 10_14.xlsx", sheet = "zRTs")
-
-healthy_errors = healthy_errors[-c(37:nrow(healthy_errors)), ]
-healthy_rts = healthy_rts[-c(37:nrow(healthy_rts)), ]
-#healthy_Zrts = healthy_Zrts[-c(37:nrow(healthy_Zrts)), ]
-
-##add age group codings
-healthy_errors$age = rep("Healthy")
-healthy_rts$age = rep("Healthy")
-#healthy_Zrts$age = rep("Healthy")
-
-####And finally MCIs####
-mci_errors = read_xlsx("Data/Older MCI Output 10_14.xlsx", sheet = "Errors")
-mci_rts = read_xlsx("Data/Older MCI Output 10_14.xlsx", sheet = "RTs")
-#mci_Zrts = read_xlsx("Data/Older MCI Output 10_14.xlsx", sheet = "zRTs")
-
-mci_errors = mci_errors[-c(27:nrow(mci_errors)), ]
-mci_rts = mci_rts[-c(27:nrow(mci_rts)), ]
-#mci_Zrts = mci_Zrts[-c(27:nrow(mci_Zrts)), ]
-
-##add age group codings
-mci_errors$age = rep("MCI")
-mci_rts$age = rep("MCI")
-#mci_Zrts$age = rep("MCI")
-
-####Combine on age group####
-errors = rbind(healthy_errors, young_errors, mci_errors)
-RTs = rbind(healthy_rts, young_rts, mci_rts)
-#ZRTs = rbind(healthy_Zrts, young_Zrts, mci_Zrts)
+errors = read.csv("Data/YA Errors.csv")
+RTs = read.csv("Data/YA RTs.csv")
 
 ####Start w/ Errors####
 summary(errors)
 
 ##need pure vs switch
-error1 = errors[ , c(1, 13, 4:8)]
-error2 = errors[ , c(1, 13, 9:12)]
+error1 = errors[ , c(1, 4:8)]
+error2 = errors[ , c(1, 9:12)]
 
 ##reshape for anovas
-error1.long = melt(data.frame(error1), id.vars = c("subID", "age"))
-error2.long = melt(data.frame(error2), id.vars = c("subID", "age"))
+error1.long = melt(data.frame(error1), id.vars = "subID")
+error2.long = melt(data.frame(error2), id.vars = "subID")
 
-colnames(error1.long)[3:4] = c("Type", "Error")
-colnames(error2.long)[3:4] = c("Type", "Error")
+colnames(error1.long)[2] = "Type"
+colnames(error2.long)[2] = "Type"
 
-##anova time!
+colnames(error1.long)[3] = "Error"
+colnames(error2.long)[3] = "Error"
+
+####ERROR ANOVAS####
+##PURE VS SWITCH
 ezANOVA(error1.long,
         within = Type,
-        between = age,
         dv = Error,
         wid = subID,
         type = 3,
-        detailed = T)
+        detailed = T) #main effect of type
 
 ##Post-hocs
-tapply(error1.long$Error, error1.long$age, mean) #main effect age
 tapply(error1.long$Error, error1.long$Type, mean) #main effect type
-tapply(error1.long$Error, list(error1.long$Type, error1.long$age), mean) #interaction
 
 ##Switch costs
 ##anova time!
 ezANOVA(error2.long,
         within = Type,
-        between = age,
         dv = Error,
         wid = subID,
         type = 3,
         detailed = T)
 
-##Post-hocs
-tapply(error2.long$Error, error2.long$age, mean) #main effect age
+####Error Post-hocs####
 tapply(error2.long$Error, error2.long$Type, mean) #main effect type
-tapply(error2.long$Error, list(error2.long$Type, error2.long$age), mean) #interaction
 
-####RTs####
+####RT ANOVAs####
 summary(RTs)
 
 ##need pure vs switch
-RTs1 = RTs[ , c(1, 13, 4:8)]
-RTs2 = RTs[ , c(1, 13, 9:12)]
+RTs1 = RTs[ , c(1, 4:8)]
+RTs2 = RTs[ , c(1, 9:12)]
 
 ##reshape for anovas
-RTs1.long = melt(data.frame(RTs1), id.vars = c("subID", "age"))
-RTs2.long = melt(data.frame(RTs2), id.vars = c("subID", "age"))
+RTs1.long = melt(data.frame(RTs1), id.vars = "subID")
+RTs2.long = melt(data.frame(RTs2), id.vars = "subID")
 
-colnames(RTs1.long)[3:4] = c("Type", "RTs")
-colnames(RTs2.long)[3:4] = c("Type", "RTs")
+colnames(RTs1.long)[2:3] = c("Type", "RTs")
+colnames(RTs2.long)[2:3] = c("Type", "RTs")
 
 ##anova time!
 ezANOVA(RTs1.long,
         within = Type,
-        between = age,
         dv = RTs,
         wid = subID,
         type = 3,
         detailed = T)
 
 ##Post-hocs
-tapply(RTs1.long$RTs, RTs1.long$age, mean) #main effect age
 tapply(RTs1.long$RTs, RTs1.long$Type, mean) #main effect type
-tapply(RTs1.long$RTs, list(RTs1.long$Type, RTs1.long$age), mean) #interaction
 
 ##Switch costs
 ##anova time!
 ezANOVA(RTs2.long,
         within = Type,
-        between = age,
         dv = RTs,
         wid = subID,
         type = 3,
         detailed = T)
 
-##Post-hocs
-tapply(RTs2.long$RTs, RTs2.long$age, mean) #main effect age
+####Rt Post-hocs####
 tapply(RTs2.long$RTs, RTs2.long$Type, mean) #main effect type
-tapply(RTs2.long$RTs, list(RTs2.long$Type, RTs2.long$age), mean) #interaction
+
+##t-tests here
--- 1 YA/2 Analyses/3 Ex Gauss/Plots/Ex_Gauss plot.py
+++ 1 YA/2 Analyses/3 Ex Gauss/Plots/Ex_Gauss plot.py
@@ -4,29 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ##read in data
-dat = pd.read_csv("ex_gauss_overall.csv")
+dat_ya = pd.read_csv("ex_gauss_ya.csv")
 
 #make the 95% confidence intervals
-dat['diff'] = dat['Upper'].sub(dat['Lower']) #get the length of the bars
-dat['diff2'] = dat['diff'].div(2) #length from end of bar to cap
-
-##split into groups bases on group
-dat_ya = dat[dat["group"] == "ya"]
-dat_healthy = dat[dat["group"] == "Healthy"]
-dat_mci = dat[dat["group"] == "MCI"]
+dat_ya['diff'] = dat_ya['Upper'].sub(dat_ya['Lower']) #get the length of the bars
+dat_ya['diff2'] = dat_ya['diff'].div(2) #length from end of bar to cap
 
 #split again based on mu, sigma, tau
 dat_ya_m = dat_ya[dat_ya["parameter"] == "mu"]
 dat_ya_s = dat_ya[dat_ya["parameter"] == "sigma"]
 dat_ya_t = dat_ya[dat_ya["parameter"] == "tau"]
 
-dat_healthy_m = dat_healthy[dat_healthy["parameter"] == "mu"]
-dat_healthy_s = dat_healthy[dat_healthy["parameter"] == "sigma"]
-dat_healthy_t = dat_healthy[dat_healthy["parameter"] == "tau"]
-
-dat_mci_m = dat_mci[dat_mci["parameter"] == "mu"]
-dat_mci_s = dat_mci[dat_mci["parameter"] == "sigma"]
-dat_mci_t = dat_mci[dat_mci["parameter"] == "tau"]
 
 ###Now get averages and conf intervals
 ##averages
@@ -40,26 +28,6 @@ dat_ya_s_average2 = dat_ya_s_average.tolist() #convert to list
 dat_ya_t_average = dat_ya_t["Average"]
 dat_ya_t_average2 = dat_ya_t_average.tolist() #convert to list
 
-#healthy
-dat_healthy_m_average = dat_healthy_m["Average"]
-dat_healthy_average_m2 = dat_healthy_m_average.tolist()
-
-dat_healthy_s_average = dat_healthy_s["Average"]
-dat_healthy_average_s2 = dat_healthy_s_average.tolist()
-
-dat_healthy_t_average = dat_healthy_t["Average"]
-dat_healthy_average_t2 = dat_healthy_t_average.tolist()
-
-#mci
-dat_mci_m_average = dat_mci_m["Average"]
-dat_mci_average_m2 = dat_mci_m_average.tolist()
-
-dat_mci_s_average = dat_mci_s["Average"]
-dat_mci_average_s2 = dat_mci_s_average.tolist()
-
-dat_mci_t_average = dat_mci_t["Average"]
-dat_mci_average_t2 = dat_mci_t_average.tolist()
-
 ##conf intervals
 #ya
 ya_m_conf = dat_ya_m["diff2"]
@@ -71,25 +39,6 @@ ya_s_conf2 = ya_s_conf.tolist() #convert to list
 ya_t_conf = dat_ya_t["diff2"]
 ya_t_conf2 = ya_t_conf.tolist() #convert to list
 
-#healthy
-healthy_m_conf = dat_healthy_m["diff2"]
-healthy_m_conf2 = healthy_m_conf.tolist() #convert to list
-
-healthy_s_conf = dat_healthy_s["diff2"]
-healthy_s_conf2 = healthy_s_conf.tolist() #convert to list
-
-healthy_t_conf = dat_healthy_t["diff2"]
-healthy_t_conf2 = healthy_t_conf.tolist() #convert to list
-
-#mci
-mci_m_conf = dat_mci_m["diff2"]
-mci_m_conf2 = mci_m_conf.tolist() #convert to list
-
-mci_s_conf = dat_mci_s["diff2"]
-mci_s_conf2 = mci_s_conf.tolist() #convert to list
-
-mci_t_conf = dat_mci_t["diff2"]
-mci_t_conf2 = mci_t_conf.tolist() #convert to list
 
 ##set up the plot
 error_fig = plt.figure()
@@ -97,16 +46,10 @@ error_fig.set_size_inches(12,20)
 
 ####FSet up plot####
 bars1 = dat_ya_m_average2
-bars2 = dat_healthy_average_m2
-bars3 = dat_mci_average_m2
 
 bars4 = dat_ya_s_average2
-bars5 = dat_healthy_average_s2
-bars6 = dat_mci_average_s2
 
 bars7 = dat_ya_t_average2
-bars8 = dat_healthy_average_t2
-bars9 = dat_mci_average_t2
 
 ##set bar width
 barwidth = 0.20 ##ax1
@@ -123,14 +66,8 @@ ax3 = error_fig.add_subplot(3, 1, 3)
 
 ####Plot Mu####
 ##make the plot
-rects1 = ax1.bar(r1, bars1, width = barwidth, yerr = ya_m_conf2, capsize = 3, color = 'w', edgecolor = 'k',
+rects1 = ax1.bar(r1, bars1, width = barwidth, yerr = ya_m_conf2, capsize = 3, color = 'gray', edgecolor = 'k',
                 label ='Younger Adults')
-
-rects2 = ax1.bar(r2, bars2, width = barwidth, yerr = healthy_m_conf2, capsize = 3, color = 'silver', edgecolor = 'k',
-                label = 'Healthy Older')
-
-rects3 = ax1.bar(r3, bars3, width = barwidth, yerr = mci_m_conf2, capsize = 3, color = 'dimgray', edgecolor = 'k',
-                label = 'MCI Older')
 
 ##Add labels, legend, and set tick marks
 ax1.set_title('Mu: Pure, Switch, and Non-Switch Trials', fontsize = 18)
@@ -144,14 +81,8 @@ ax1.legend()
 
 ####Plot sigma####
 ##make the plot
-rects1 = ax2.bar(r1, bars4, width = barwidth, yerr = ya_s_conf2, capsize = 3, color = 'w', edgecolor = 'k',
+rects1 = ax2.bar(r1, bars4, width = barwidth, yerr = ya_s_conf2, capsize = 3, color = 'gray', edgecolor = 'k',
                 label ='Younger Adults')
-
-rects2 = ax2.bar(r2, bars5, width = barwidth, yerr = healthy_s_conf2, capsize = 3, color = 'silver', edgecolor = 'k',
-                label = 'Healthy Older')
-
-rects3 = ax2.bar(r3, bars6, width = barwidth, yerr = mci_s_conf2, capsize = 3, color = 'dimgray', edgecolor = 'k',
-                label = 'MCI Older')
 
 ##Add labels, legend, and set tick marks
 ax2.set_title('Sigma: Pure, Switch, and Non-Switch Trials', fontsize = 18)
@@ -166,14 +97,8 @@ ax2.legend(loc = "upper left")
 ##Tau
 ####Plot Tau####
 ##make the plot
-rects1 = ax3.bar(r1, bars7, width = barwidth, yerr = ya_t_conf2, capsize = 3, color = 'w', edgecolor = 'k',
+rects1 = ax3.bar(r1, bars7, width = barwidth, yerr = ya_t_conf2, capsize = 3, color = 'gray', edgecolor = 'k',
                 label ='Younger Adults')
-
-rects2 = ax3.bar(r2, bars8, width = barwidth, yerr = healthy_t_conf2, capsize = 3, color = 'silver', edgecolor = 'k',
-                label = 'Healthy Older')
-
-rects3 = ax3.bar(r3, bars9, width = barwidth, yerr = mci_t_conf2, capsize = 3, color = 'dimgray', edgecolor = 'k',
-                label = 'MCI Older')
 
 ##Add labels, legend, and set tick marks
 ax3.set_title('Tau: Pure, Switch, and Non-Switch Trials', fontsize = 18)
