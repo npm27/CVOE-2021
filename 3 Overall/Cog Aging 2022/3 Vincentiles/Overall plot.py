eaf994f6c76e436b86846623266a7e261d4a7fc5
npm27 <nick.maxwell61@yahoo.com>
2022-03-02 16:19:15 -0600

    Vincentile Plots

--- 3 Overall/Cog Aging 2022/3 Vincentiles/Overall plot.py
+++ 3 Overall/Cog Aging 2022/3 Vincentiles/Overall plot.py
@@ -3,72 +3,74 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dat = pd.read_csv("vincentiles.csv")
+dat = pd.read_csv("vincentiles Costs 3_2_22.csv")
 
 dat['diff'] = dat['Upper'].sub(dat['Lower'])
 dat['diff2'] = dat['diff'].div(2)
 
 ##set up the initial plot
 fig = plt.figure()
-fig.set_size_inches(18,30)
+fig.set_size_inches(30,20)
 
-fig.subplots_adjust(hspace=.5)
+fig.subplots_adjust(hspace=.30)
 
 ##Make the subplots
-ax1 = fig.add_subplot(5, 1, 1)
-ax2 = fig.add_subplot(5, 1, 2)
-ax3 = fig.add_subplot(5, 1, 3)
-ax4 = fig.add_subplot(5, 1, 4)
-ax5 = fig.add_subplot(5, 1, 5)
+ax1 = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2)
+ax3 = fig.add_subplot(2, 2, 3)
+ax4 = fig.add_subplot(2, 2, 4)
+
 
 ##First subset on trial type
-pure = dat[dat['Trial_Type'] == "pure"]
-alt_ns = dat[dat['Trial_Type'] == "alt_ns"]
-alt_switch = dat[dat['Trial_Type'] == "alt_switch"]
-rand_ns = dat[dat['Trial_Type'] == "rand_ns"]
-rand_switch = dat[dat['Trial_Type'] == "rand_switch"]
+alt_G = dat[dat['Trial Type'] == "Alt_Global"]
+alt_L = dat[dat['Trial Type'] == "Alt_Local"]
+rand_G = dat[dat['Trial Type'] == "Rand_Global"]
+rand_L = dat[dat['Trial Type'] == "Rand_Local"]
 
 ###Start w/ the first plot
 ##Each plot will have one trial type for each age group
 #Pure
-pure_ya = pure[pure['Group'] == 'YA']
-pure_h = pure[pure['Group'] == 'Healthy']
-pure_mci = pure[pure['Group'] == 'MCI']
+
+##Alt-Local
+temp_ya = alt_L[alt_L['Group'] == 'YA']
+temp_h = alt_L[alt_L['Group'] == 'Healthy']
+temp_mci = alt_L[alt_L['Group'] == 'MCI']
 
 #YA
-x1 = pure_ya.bin.values
-y1 = pure_ya.Average.values
+x1 = temp_ya.bin.values
+y1 = temp_ya.Average.values
 
 ax1.plot(x1, y1, marker = '.', color = 'k', label='Younger')
-ax1.errorbar(x1, y1, yerr = (pure_ya['diff2']), fmt='none', c= 'k', capsize=5)
+ax1.errorbar(x1, y1, yerr = (temp_ya['diff2']), fmt='none', c= 'k', capsize=5)
 
 #HEALTHY
-x2 = pure_h.bin.values
-y2 = pure_h.Average.values
+x2 = temp_h.bin.values
+y2 = temp_h.Average.values
 
 ax1.plot(x2, y2, marker = 's', color = 'navy', label='Healthy')
-ax1.errorbar(x2, y2, yerr = (pure_h['diff2']), fmt='none', c= 'navy', capsize=5)
+ax1.errorbar(x2, y2, yerr = (temp_h['diff2']), fmt='none', c= 'navy', capsize=5)
 
 ##MCI
-x3 = pure_mci.bin.values
-y3 = pure_mci.Average.values
+x3 = temp_mci.bin.values
+y3 = temp_mci.Average.values
 
 ax1.plot(x3, y3, marker = 'v', color = 'dodgerblue', label='MCI')
-ax1.errorbar(x3, y3, yerr = (pure_mci['diff2']), fmt='none', c= 'dodgerblue', capsize=5)
+ax1.errorbar(x3, y3, yerr = (temp_mci['diff2']), fmt='none', c= 'dodgerblue', capsize=5)
 
 ##Make the plot spiffy
-ax1.set_title('Pure Trials', fontsize = 20, fontweight = 'bold')
-ax1.set_ylabel('Mean RT (ms)', fontsize = 18)
-ax1.set_xlabel('Vincentile Bin', fontsize = 18)
-ax1.tick_params(axis='x', labelsize = 16)
-ax1.tick_params(axis='y', labelsize = 16)
+ax1.set_title('Alternating Runs', fontsize = 30, fontweight = 'bold')
+ax1.set_ylabel('Mean RT (ms)', fontsize = 26)
+ax1.set_xlabel('Vincentile Bin', fontsize = 26)
+ax1.tick_params(axis='x', labelsize = 23)
+ax1.tick_params(axis='y', labelsize = 23)
 
-ax1.legend()
+#ax1.set_ylim([0, 2500])
+ax1.legend(borderaxespad = 1, fontsize = 14, frameon = False)
 
-##Alt-Non-switch
-temp_ya = alt_ns[alt_ns['Group'] == 'YA']
-temp_h = alt_ns[alt_ns['Group'] == 'Healthy']
-temp_mci = alt_ns[alt_ns['Group'] == 'MCI']
+##Random Local
+temp_ya = rand_L[rand_L['Group'] == 'YA']
+temp_h = rand_L[rand_L['Group'] == 'Healthy']
+temp_mci = rand_L[rand_L['Group'] == 'MCI']
 
 #YA
 x1 = temp_ya.bin.values
@@ -92,18 +94,19 @@ ax2.plot(x3, y3, marker = 'v', color = 'dodgerblue', label='MCI')
 ax2.errorbar(x3, y3, yerr = (temp_mci['diff2']), fmt='none', c= 'dodgerblue', capsize=5)
 
 ##Make the plot spiffy
-ax2.set_title('Alt-Runs Non-Switch Trials', fontsize = 20, fontweight = 'bold')
-ax2.set_ylabel('Mean RT (ms)', fontsize = 18)
-ax2.set_xlabel('Vincentile Bin', fontsize = 18)
-ax2.tick_params(axis='x', labelsize = 16)
-ax2.tick_params(axis='y', labelsize = 16)
+ax2.set_title('Random', fontsize = 30, fontweight = 'bold')
+ax2.set_ylabel('Mean RT (ms)', fontsize = 26)
+ax2.set_xlabel('Vincentile Bin', fontsize = 26)
+ax2.tick_params(axis='x', labelsize = 23)
+ax2.tick_params(axis='y', labelsize = 23)
 
-ax2.legend()
+#ax2.set_ylim([0, 2500])
+ax2.legend(borderaxespad = 1, fontsize = 14, frameon = False)
 
-##Rand NS
-temp_ya = rand_ns[rand_ns['Group'] == 'YA']
-temp_h = rand_ns[rand_ns['Group'] == 'Healthy']
-temp_mci = rand_ns[rand_ns['Group'] == 'MCI']
+##Alt Global
+temp_ya = alt_G[alt_G['Group'] == 'YA']
+temp_h = alt_G[alt_G['Group'] == 'Healthy']
+temp_mci = alt_G[alt_G['Group'] == 'MCI']
 
 #YA
 x1 = temp_ya.bin.values
@@ -127,18 +130,19 @@ ax3.plot(x3, y3, marker = 'v', color = 'dodgerblue', label='MCI')
 ax3.errorbar(x3, y3, yerr = (temp_mci['diff2']), fmt='none', c= 'dodgerblue', capsize=5)
 
 ##Make the plot spiffy
-ax3.set_title('Random Non-Switch Trials', fontsize = 20, fontweight = 'bold')
-ax3.set_ylabel('Mean RT (ms)', fontsize = 18)
-ax3.set_xlabel('Vincentile Bin', fontsize = 18)
-ax3.tick_params(axis='x', labelsize = 16)
-ax3.tick_params(axis='y', labelsize = 16)
+ax3.set_title('Alternating Runs', fontsize = 30, fontweight = 'bold')
+ax3.set_ylabel('Mean RT (ms)', fontsize = 26)
+ax3.set_xlabel('Vincentile Bin', fontsize = 26)
+ax3.tick_params(axis='x', labelsize = 23)
+ax3.tick_params(axis='y', labelsize = 23)
 
-ax3.legend()
+ax3.set_ylim([0, 2500])
+ax3.legend(borderaxespad = 1, fontsize = 14, frameon = False)
 
-##Alt switch
-temp_ya = alt_switch[alt_switch['Group'] == 'YA']
-temp_h = alt_switch[alt_switch['Group'] == 'Healthy']
-temp_mci = alt_switch[alt_switch['Group'] == 'MCI']
+##Rand GLOBAL
+temp_ya = rand_G[rand_G['Group'] == 'YA']
+temp_h = rand_G[rand_G['Group'] == 'Healthy']
+temp_mci = rand_G[rand_G['Group'] == 'MCI']
 
 #YA
 x1 = temp_ya.bin.values
@@ -162,48 +166,16 @@ ax4.plot(x3, y3, marker = 'v', color = 'dodgerblue', label='MCI')
 ax4.errorbar(x3, y3, yerr = (temp_mci['diff2']), fmt='none', c= 'dodgerblue', capsize=5)
 
 ##Make the plot spiffy
-ax4.set_title('Alt-Runs Switch Trials', fontsize = 20, fontweight = 'bold')
-ax4.set_ylabel('Mean RT (ms)', fontsize = 18)
-ax4.set_xlabel('Vincentile Bin', fontsize = 18)
-ax4.tick_params(axis='x', labelsize = 16)
-ax4.tick_params(axis='y', labelsize = 16)
+ax4.set_title('Random', fontsize = 30, fontweight = 'bold')
+ax4.set_ylabel('Mean RT (ms)', fontsize = 26)
+ax4.set_xlabel('Vincentile Bin', fontsize = 26)
+ax4.tick_params(axis='x', labelsize = 23)
+ax4.tick_params(axis='y', labelsize = 23)
 
-ax4.legend()
+ax4.set_ylim([0, 2300])
+ax4.legend(borderaxespad = 1, fontsize = 14, frameon = False)
 
-##Rand Switch
-temp_ya = rand_switch[rand_switch['Group'] == 'YA']
-temp_h = rand_switch[rand_switch['Group'] == 'Healthy']
-temp_mci = rand_switch[rand_switch['Group'] == 'MCI']
-
-#YA
-x1 = temp_ya.bin.values
-y1 = temp_ya.Average.values
-
-ax5.plot(x1, y1, marker = '.', color = 'k', label='Younger')
-ax5.errorbar(x1, y1, yerr = (temp_ya['diff2']), fmt='none', c= 'k', capsize=5)
-
-#HEALTHY
-x2 = temp_h.bin.values
-y2 = temp_h.Average.values
-
-ax5.plot(x2, y2, marker = 's', color = 'navy', label='Healthy')
-ax5.errorbar(x2, y2, yerr = (temp_h['diff2']), fmt='none', c= 'navy', capsize=5)
-
-##MCI
-x3 = temp_mci.bin.values
-y3 =temp_mci.Average.values
-
-ax5.plot(x3, y3, marker = 'v', color = 'dodgerblue', label='MCI')
-ax5.errorbar(x3, y3, yerr = (temp_mci['diff2']), fmt='none', c= 'dodgerblue', capsize=5)
-
-##Make the plot spiffy
-ax5.set_title('Random Switch Trials', fontsize = 20, fontweight = 'bold')
-ax5.set_ylabel('Mean RT (ms)', fontsize = 18)
-ax5.set_xlabel('Vincentile Bin', fontsize = 18)
-ax5.tick_params(axis='x', labelsize = 16)
-ax5.tick_params(axis='y', labelsize = 16)
-
-ax5.legend()
+#fig.legend()
 
 ##save
-fig.savefig('Overall_Vincentiles.png')
+fig.savefig('Vincentile Costs.png')
